15651ad7d41d360d0818768f7bbfe4a8f750ed86
Zijian-Wang <ryegogogo@gmail.com>
2023-01-08 14:46:13 +0800

    Tried suspense loading
    But not finished

--- Website/alfa-romeo-33-website/src/components/hl-Collections.jsx
+++ Website/alfa-romeo-33-website/src/components/hl-Collections.jsx
@@ -12,11 +12,28 @@ export default function HLCollections(params) {
   camera.rotation.set(-0.34, -0.41, -0.14)
 
   // const controls = useControls({
+  //   position: {
+  //     value: [-0.68, 0.05, 1.65],
+  //     step: 0.01,
+  //   },
   //   rotation: {
-  //     value: [0, 0, 0],
+  //     value: [-0.07, -0.62, -0.03],
   //     step: 0.01,
   //   },
   // })
+
+  function fallbackLoading() {
+    return (
+      <>
+        <color attach="background" args={['#000']} />
+        {/* <mesh position={[-0.68, 0.05, 1.65]} rotation={[-0.07, -0.62, -0.03]}>
+          <boxGeometry args={[1, 1, 1, 2, 2, 2]} />
+          <meshBasicMaterial wireframe color="red" />
+        </mesh> */}
+      </>
+    )
+  }
+
   return (
     <>
       <StrictMode>
@@ -32,11 +49,13 @@ export default function HLCollections(params) {
             polar={[-Math.PI / 8, Math.PI / 8]}
             azimuth={[-Math.PI / 5, Math.PI / 5]}
           >
-            <Suspense fallback={null}>
+            <Suspense fallback={fallbackLoading()}>
               <CarCollection
                 scale={1}
-                position={[-0.3, 0.5, 0]}
-                rotation={[-0.1, 0, 0]}
+                position={[-0.68, 0.05, 1.65]}
+                rotation={[-0.07, -0.62, -0.03]}
+                // position={[-0.3, 0.5, 0]}
+                // rotation={[-0.12, -0.6, 0]}
               />
             </Suspense>
           </PresentationControls>
